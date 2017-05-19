5be58fe40108596ff9f05bec7d1a196e15d77a53
Ali <ali.kholmatov@gmail.com>
2017-05-20 00:51:09 +0300

    update slider

--- js/changeRadioButton.js
+++ js/changeRadioButton.js
@@ -5,28 +5,35 @@ let radioElements = document.querySelectorAll("input[name=toogle]");
 let sliderOverflow = document.querySelector(".slider-overflow");
 let step = 1;
 
+for (let i = 0; radioElements.length > i; i++) {
+    radioElements[i].addEventListener("change", function () {
+        sliderOverflow.style.transform = "translate(" + (-(i * document.body.clientWidth)) + "px)";
+    });
+}
 sliderOverflow.addEventListener("transitionend", function (event) {
-    if (sliderOverflow.classList.contains("elemopacity")) {
-        change();
+    if (sliderOverflow.classList.contains("slider-opacity")) {
+       // change();
     }
 });
 
 let timeId = setInterval(function () {
-    sliderOverflow.classList.add("slider-overflow-opacity");
+    sliderOverflow.classList.add("slider-opacity");
 }, 10000);
 
 setInterval(function () {
-    sliderOverflow.classList.remove("slider-overflow-opacity");
+    sliderOverflow.classList.remove("slider-opacity");
 }, 11000);
 
 function change() {
     if (radioElements.length > step) {
-        radioElements[step].checked = true;
+        //radioElements[step].checked = true;
+        //transformSlider(step);
         step++;
     }
     else {
         step = 1;
-        radioElements[step - 1].checked = true;
+        //radioElements[step - 1].checked = true;
+        //transformSlider(step-1);
     }
 
 }
