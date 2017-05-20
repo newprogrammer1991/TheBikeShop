3982ee286febcc429d9b46b30a2d331a1cdca3b6
Ali <ali.kholmatov@gmail.com>
2017-05-20 19:00:19 +0300

    update slider

--- js/changeRadioButton.js
+++ js/changeRadioButton.js
@@ -1,39 +1,146 @@
+(function () {
+    let radioButtons = document.querySelectorAll("input[name=toogle]");
+    let sliderOverflow=document.querySelector(".slider-overflow");
+
+    for (let i=0;radioButtons.length;i++){
+        radioButtons[i].addEventListener("change",function () {
+        changeSlider(i)
+        })
+    }
+
+    function changeSlider(i) {
+        sliderOverflow.style.transform = "translate(" + (-(i * document.body.clientWidth)) + "px)";
+    }
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(function () {
+    let elem = document.querySelectorAll(".slider-navigation label");
+    let radioButtons = document.querySelectorAll("input[name=toogle]");
+    let sliderOverflow=document.querySelector(".slider-overflow");
+    let sliders=document.querySelectorAll(".slider");
+    let next;
+    let radioCheckedNumber
+    for (let i = 0; radioButtons.length > i; i++) {
+        radioButtons[i].addEventListener("change", function () {
+        addClass(i);
+        })
+    }
+    for (let i=0;sliders.length>i;i++) {
+        sliders[i].addEventListener("transitionend", function () {
+            if(sliders[i].contains("slider-opacity")) {
+                transforms(i + 1);
+            }
+        })
+    }
+
+    function addClass(i) {
+
+        if(sliders[i].contains("slider-opacity")){
+           sliders[i].classList.remove("slider-opacity");
+           sliders[i+radioCheckedNumber].classList.add("slider-opacity");
+        }
+
+        else {
+            sliders[i - 1].classList.add("slider-opacity");
+        }
+    };
+function transforms(i) {
+    console.log(i);
+    sliderOverflow.style.transform = "translate(" + (-(i * document.body.clientWidth)) + "px)";
+};
+    function getCheckedButton() {
+        for (let i=0;radioButtons.length>i;i++){
+            if(radioButtons[i].checked==true){
+
+            }
+        }
+            }
+
+})();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created by ALI on 5/18/2017.
  */
-let radioElements = document.querySelectorAll("input[name=toogle]");
-let sliderOverflow = document.querySelector(".slider-overflow");
-let step = 1;
+/*let radioElements = document.querySelectorAll("input[name=toogle]");
+ let sliderOverflow = document.querySelector(".slider-overflow");
+ let step = 1;
 
-for (let i = 0; radioElements.length > i; i++) {
-    radioElements[i].addEventListener("change", function () {
-        sliderOverflow.style.transform = "translate(" + (-(i * document.body.clientWidth)) + "px)";
-    });
-}
-sliderOverflow.addEventListener("transitionend", function (event) {
-    if (sliderOverflow.classList.contains("slider-opacity")) {
-       // change();
-    }
-});
+ for (let i = 0; radioElements.length > i; i++) {
+ radioElements[i].addEventListener("change", function () {
+ transformSlider(i);
 
-let timeId = setInterval(function () {
-    sliderOverflow.classList.add("slider-opacity");
-}, 10000);
+ });
+ }
 
-setInterval(function () {
-    sliderOverflow.classList.remove("slider-opacity");
-}, 11000);
+ function transformSlider(i) {
+ sliderOverflow.style.transform = "translate(" + (-(i * document.body.clientWidth)) + "px)";
+ sliderOverflow.classList.add("slider-opacity");
+ }
 
-function change() {
-    if (radioElements.length > step) {
-        //radioElements[step].checked = true;
-        //transformSlider(step);
-        step++;
-    }
-    else {
-        step = 1;
-        //radioElements[step - 1].checked = true;
-        //transformSlider(step-1);
-    }
+ sliderOverflow.addEventListener("transitionend", function (event) {
+ if (sliderOverflow.classList.contains("slider-opacity")) {
+ sliderOverflow.classList.remove("slider-opacity");
+ //change();
+ }
+ });
 
-}
+ let timeId = setInterval(function () {
+ sliderOverflow.classList.add("slider-opacity");
+ }, 10000);
+
+ setInterval(function () {
+ sliderOverflow.classList.remove("slider-opacity");
+ }, 11000);
+
+ function change() {
+ if (radioElements.length > step) {
+ radioElements[step].checked = true;
+ transformSlider(step);
+ step++;
+ }
+ else {
+ step = 1;
+ radioElements[step - 1].checked = true;
+ transformSlider(step-1);
+ }
+
+ }*/
--- js/modification-htmlselect.js
+++ js/modification-htmlselect.js
@@ -1,45 +1,48 @@
-let clicksForSelect = document.querySelectorAll(".wrapper-select");
-let btnOptions = document.querySelectorAll(".select-btn");
-let elemLastClick;
-for (let i = 0; clicksForSelect.length > i; i++) {
-    clicksForSelect[i].addEventListener("click", show);
-}
-for (let i = 0; btnOptions.length > i; i++) {
-    btnOptions[i].addEventListener("click", choose);
-}
+(function () {
+    let clicksForSelect = document.querySelectorAll(".wrapper-select");
+    let btnOptions = document.querySelectorAll(".select-btn");
+    let elemLastClick;
+    for (let i = 0; clicksForSelect.length > i; i++) {
+        clicksForSelect[i].addEventListener("click", show);
+    }
+    for (let i = 0; btnOptions.length > i; i++) {
+        btnOptions[i].addEventListener("click", choose);
+    }
 
-function show() {
-    let elem = this;
-    if (document.querySelector(".show") && elemLastClick != elem) {
-        document.querySelector(".show").classList.remove("show");
-    }
-    elemLastClick = this;
-    let selectNone = document.querySelectorAll(".select-list");
-    for (let i = 0; selectNone.length > i; i++) {
-        if (elem.contains(selectNone[i])) {
-            selectNone[i].classList.toggle("show");
+    function show() {
+        let elem = this;
+        if (document.querySelector(".show") && elemLastClick != elem) {
+            document.querySelector(".show").classList.remove("show");
         }
-    }
-}
-function choose() {
-    let elem = this;
-    let textChange = elem.value;
-    if(textChange) {
-        let spanTextChange = document.querySelectorAll(".text-change");
-        for (let i = 0; spanTextChange.length > i; i++) {
-            if (elemLastClick.contains(spanTextChange[i])) {
-                spanTextChange[i].innerHTML = textChange;
+        elemLastClick = this;
+        let selectNone = document.querySelectorAll(".select-list");
+        for (let i = 0; selectNone.length > i; i++) {
+            if (elem.contains(selectNone[i])) {
+                selectNone[i].classList.toggle("show");
             }
         }
     }
-}
 
-
-document.addEventListener("click", function () {
-    if (document.querySelector(".show") && !event.target.closest(".wrapper-select")) {
-        document.querySelector(".show").classList.remove("show");
+    function choose() {
+        let elem = this;
+        let textChange = elem.value;
+        if (textChange) {
+            let spanTextChange = document.querySelectorAll(".text-change");
+            for (let i = 0; spanTextChange.length > i; i++) {
+                if (elemLastClick.contains(spanTextChange[i])) {
+                    spanTextChange[i].innerHTML = textChange;
+                }
+            }
+        }
     }
 
-});
+
+    document.addEventListener("click", function () {
+        if (document.querySelector(".show") && !event.target.closest(".wrapper-select")) {
+            document.querySelector(".show").classList.remove("show");
+        }
+
+    });
 
 
+})();
